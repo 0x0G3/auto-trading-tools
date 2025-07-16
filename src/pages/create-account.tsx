b724e3cbb110b875f1c245493edf5494cb90af1d
george <george@trustware.io>
2025-07-16 15:41:07 -0700

    restructing apis/db

---
+++ src/pages/create-account.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CreateAccount from "../components/CreateAccount";
+
+export default function createaccount() {
+  return (
+    <div>
+      <CreateAccount />
+    </div>
+  );
+}
